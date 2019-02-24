e1e05a1a71677c1bf7d79a5794c333748cc3ab08
debruine <debruine@gmail.com>
2019-02-24 16:59:50 +0000

    abstracted out cormat() and more tests

--- R/rnorm_multi.R
+++ R/rnorm_multi.R
@@ -1,44 +1,20 @@
-#' Multiple Normally Distributed Vectors
+#' Make Correlation Matrix
 #'
-#' \code{rnorm_multi} makes multiple normally distributed vectors with specified relationships
+#' \code{cormat} makes a correlation matrix from a vector
 #'
-#' @param n the number of samples required
-#' @param vars the number of variables to return
 #' @param cors the correlations among the variables (can be a single number, vars\*vars matrix, vars\*vars vector, or a vars\*(vars-1)/2 vector)
-#' @param mu a vector giving the means of the variables (numeric vector of length 1 or vars)
-#' @param sd the standard deviations of the variables (numeric vector of length 1 or vars)
-#' @param varnames optional names for the variables (string vector of length vars)
-#' @param empirical logical. If true, mu, sd and cors specify the empirical not population mean, sd and covariance 
+#' @param vars the number of variables in the matrix
 #' 
-#' @return dataframe of vars vectors
+#' @return matrix
 #' @examples
-#' rnorm_multi(100, 3, c(0.2, 0.4, 0.5), varnames=c("A", "B", "C"))
-#' rnorm_multi(100, 3, c(1, 0.2, -0.5, 0.2, 1, 0.5, -0.5, 0.5, 1), varnames=c("A", "B", "C"))
+#' cormat(.5, 3)
+#' cormat(c( 1, .2, .3, .4,
+#'          .2,  1, .5, .6, 
+#'          .3, .5,  1, .7,
+#'          .4, .6, .7,  1), 4)
+#' cormat(c(.2, .3, .4, .5, .6, .7), 4)
 #' @export
-
-rnorm_multi <- function(n, vars = 3, cors = 0, mu = 0, sd = 1, 
-                       varnames = NULL, empirical = FALSE) {
-  # error handling
-  if ( !is.numeric(n) || n %% 1 > 0 || n < 3 ) {
-    stop("n must be an integer > 2")
-  }
-  
-  if (!(empirical  %in% c(TRUE, FALSE))) {
-    stop("empirical must be TRUE or FALSE")
-  }
-  
-  if (length(mu) == 1) {
-    mu <- rep(mu, vars)
-  } else if (length(mu) != vars) {
-    stop("the length of mu must be 1 or vars");
-  }
-  
-  if (length(sd) == 1) {
-    sd <- rep(sd, vars)
-  } else if (length(sd) != vars) {
-    stop("the length of sd must be 1 or vars");
-  }
-  
+cormat <- function(cors = 0, vars = 3) {
   # correlation matrix
   if (class(cors) == "numeric" & length(cors) == 1) {
     if (cors >=-1 & cors <=1) {
@@ -95,6 +71,52 @@ rnorm_multi <- function(n, vars = 3, cors = 0, mu = 0, sd = 1,
   if (sum(ev < tol)) {
     stop("correlation matrix not positive definite")
   }
+  
+  return(cor_mat)
+}
+
+#' Multiple Normally Distributed Vectors
+#'
+#' \code{rnorm_multi} makes multiple normally distributed vectors with specified relationships
+#'
+#' @param n the number of samples required
+#' @param vars the number of variables to return
+#' @param cors the correlations among the variables (can be a single number, vars\*vars matrix, vars\*vars vector, or a vars\*(vars-1)/2 vector)
+#' @param mu a vector giving the means of the variables (numeric vector of length 1 or vars)
+#' @param sd the standard deviations of the variables (numeric vector of length 1 or vars)
+#' @param varnames optional names for the variables (string vector of length vars)
+#' @param empirical logical. If true, mu, sd and cors specify the empirical not population mean, sd and covariance 
+#' 
+#' @return dataframe of vars vectors
+#' @examples
+#' rnorm_multi(100, 3, c(0.2, 0.4, 0.5), varnames=c("A", "B", "C"))
+#' rnorm_multi(100, 3, c(1, 0.2, -0.5, 0.2, 1, 0.5, -0.5, 0.5, 1), varnames=c("A", "B", "C"))
+#' @export
+
+rnorm_multi <- function(n, vars = 3, cors = 0, mu = 0, sd = 1, 
+                       varnames = NULL, empirical = FALSE) {
+  # error handling
+  if ( !is.numeric(n) || n %% 1 > 0 || n < 3 ) {
+    stop("n must be an integer > 2")
+  }
+  
+  if (!(empirical  %in% c(TRUE, FALSE))) {
+    stop("empirical must be TRUE or FALSE")
+  }
+  
+  if (length(mu) == 1) {
+    mu <- rep(mu, vars)
+  } else if (length(mu) != vars) {
+    stop("the length of mu must be 1 or vars");
+  }
+  
+  if (length(sd) == 1) {
+    sd <- rep(sd, vars)
+  } else if (length(sd) != vars) {
+    stop("the length of sd must be 1 or vars");
+  }
+  
+  cor_mat <- cormat(cors, vars)
   
   sigma <- (sd %*% t(sd)) * cor_mat
   bvn <- MASS::mvrnorm(n, mu, sigma, empirical = empirical)
---
+++ tests/testthat/test-cormat.R
@@ -0,0 +1,68 @@
+context("cormat")
+
+test_that("errors", {
+  expect_error(
+    cormat(matrix("A", 3, 3)),
+    "cors matrix not numeric" 
+  )
+  expect_error(
+    cormat(matrix(0.5, 4, 2)),
+    "cors matrix wrong dimensions" 
+  )
+  
+  m <- matrix(c(1, .5, .5, .5, 1, .5, .5, .75, 1), 3)
+  expect_error( 
+    cormat(m), 
+    "cors matrix not symmetric"
+  )
+  
+  m <- matrix(c(1, .5, .5, .5, 1, .5, .5, .5, 0), 3)
+  expect_error(
+    cormat(m),
+    "correlation matrix not positive definite"
+  )
+  
+  cors <- c(-0.06826927, -0.89756943, -0.45636273)
+  expect_error(
+    cormat(cors),
+    "correlation matrix not positive definite"
+  )
+})
+
+test_that("correct matrix", {
+  # specify by single value
+  mat1 <- cormat(.5, 3)
+  compmat1 <- matrix(c(1, .5, .5, .5, 1, .5, .5, .5, 1), 3)
+  expect_equal(mat1, compmat1)
+  
+  # specify by vars*vars vector
+  mat2 <- cormat(c( 1, .2, .3, .4,
+                   .2,  1, .5, .6, 
+                   .3, .5,  1, .7,
+                   .4, .6, .7,  1), 4)
+  # specify by vars*(vars-1)/2 vector
+  mat3 <- cormat(c(.2, .3, .4, .5, .6, .7), 4)
+  expect_equal(mat2, mat3)
+  
+  compmat2 <- matrix(c(  1, .2, .3, .4,
+                        .2,  1, .5, .6, 
+                        .3, .5,  1, .7,
+                        .4, .6, .7,  1), 4)
+  expect_equal(mat2, compmat2)
+  
+  # larger vector
+  mat4 <- cormat(c(.2, .3, .4, -.45, .5, .6, -.65, .7, -.75, -.85), 5)
+  compmat4 <- matrix(c(  1, .2, .3, .4, -.45,
+                         .2,  1, .5, .6, -.65,
+                         .3, .5,  1, .7, -.75,
+                         .4, .6, .7,  1, -.85,
+                         -.45,-.65,-.75,-.85, 1), 5)
+  expect_equal(mat4, compmat4)
+  
+  # specify by matrix
+  mat5 <- cormat(compmat4, 5)
+  expect_equal(mat5, compmat4)
+})
+  
+  
+  
