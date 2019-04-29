9c2a200ff458986534f9cd185d2cfe13e7f76fda
debruine <debruine@gmail.com>
2019-04-29 17:04:08 +0100

    Speed test for anova

--- data-raw/speed_tests.R
+++ data-raw/speed_tests.R
@@ -1,8 +1,12 @@
+library(faux)
+library(tidyverse)
+
+
 # speed test ----
 
-library(tidyverse)
+
 within <- list(
-  "A" = c("A1", "A2")
+  "W" = c("W1", "W2")
 )
 
 between <- list(
@@ -14,20 +18,30 @@ mu <- list(
   "B2" = c(10, 10)
 )
 
-func <- function(i) {
+anova_func <- function(i) {
   utils::setTxtProgressBar(pb, i)
   df <- sim_design(within, between, n = 20, mu = mu, frame_long = TRUE)
-  suppressMessages(
-    anova <- afex::aov_4(val~B*(A|sub_id), data = df, check_contrasts = TRUE)
-  )
-  anova$anova_table %>%
-    tibble::as_tibble(rownames = "factor")
+  aov <- aov(val~B*W+Error(sub_id/W), data = df, contrasts = NULL) %>% 
+    summary() %>% 
+    unclass()
+  
+  b <- aov[[1]][[1]] %>% tibble::as_tibble(rownames = "term")
+  w <- aov[[2]][[1]] %>% tibble::as_tibble(rownames = "term")
+  b$`den Df` <- b$Df[length(b$Df)]
+  w$`den Df` <- w$Df[length(w$Df)]
+  
+  dplyr::bind_rows(b, w) %>%
+    dplyr::filter(term != "Residuals") %>%
+    dplyr::mutate(term = trimws(term)) %>%
+    dplyr::select(`term`, `num Df` = `Df`, `den Df`, 
+                  `Sum Sq`, `Mean Sq`, `F` = `F value`, 
+                  `p` = `Pr(>F)`)
 }
 
-reps <- 2
+reps <- 10000
 pb <- txtProgressBar(max = reps)
 timestamp()
-sims <- purrr::map_df(1:reps, func)
+sims <- purrr::map_df(1:reps, anova_func)
 timestamp()
 close(pb)
 
@@ -47,4 +61,8 @@ sims %>%
 ##------ Sun Apr 28 20:43:15 2019 ------##
 #   df <- purrr::map(1:1e4, ~sim_design(within, between, n = 20))
 ##------ Sun Apr 28 20:44:48 2019 ------##
+
+##------ Mon Apr 29 16:59:53 2019 ------##
+# > sims <- purrr::map_df(1:1e4, anova_func)
+##------ Mon Apr 29 17:02:51 2019 ------##
 
