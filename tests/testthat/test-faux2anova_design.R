3fa276e282696d2e747a87c052e70463856dec48
debruine <debruine@gmail.com>
2019-12-10 09:52:57 +0100

    Removed ANOVApower

--- tests/testthat/test-faux2anova_design.R
+++ tests/testthat/test-faux2anova_design.R
@@ -17,14 +17,15 @@ test_that("2w", {
   ap <- faux2ANOVA_design(design, plot = FALSE)
   
   expect_equal(ap$design, "2w")
-  expect_equal(ap$design_list, c("A1", "A2"))
-  expect_equal(ap$factors, 1)
+  #expect_equal(ap$design_list, c("A1", "A2"))
+  #expect_equal(ap$factors, 1)
   expect_equal(ap$mu, c(0,0))
   expect_equal(ap$sd, c(1,1))
   expect_equal(ap$r, 0)
   expect_equal(ap$n, 100)
-  expect_equal(ap$design_factors, 1)
+  #expect_equal(ap$design_factors, 1)
   expect_equal(ap$labelnames, c("A", "A1", "A2"))
+  expect_equal(ap$plot, FALSE)
 })
 
 # 2b ----
@@ -33,14 +34,15 @@ test_that("2b", {
   ap <- faux2ANOVA_design(design, plot = FALSE)
   
   expect_equal(ap$design, "2b")
-  expect_equal(ap$design_list, c("A1", "A2"))
-  expect_equal(ap$factors, 1)
+  #expect_equal(ap$design_list, c("A1", "A2"))
+  #expect_equal(ap$factors, 1)
   expect_equal(ap$mu, c(0,0))
   expect_equal(ap$sd, c(1,1))
   expect_equal(ap$r, 0)
   expect_equal(ap$n, 100)
-  expect_equal(ap$design_factors, 0)
+  #expect_equal(ap$design_factors, 0)
   expect_equal(ap$labelnames, c("A", "A1", "A2"))
+  expect_equal(ap$plot, FALSE)
 })
 
 # 2w*2w*2b ----
@@ -63,10 +65,10 @@ test_that("2w*2w*2b", {
   ap <- faux2ANOVA_design(des, plot = FALSE)
   
   expect_equal(ap$design, "2b*2w*2w")
-  expect_equal(ap$design_list, c(
-    "dog_night_A", "dog_night_B", "dog_day_A", "dog_day_B", 
-    "cat_night_A", "cat_night_B", "cat_day_A", "cat_day_B"))
-  expect_equal(ap$factors, 3)
+  # expect_equal(ap$design_list, c(
+  #   "dog_night_A", "dog_night_B", "dog_day_A", "dog_day_B", 
+  #   "cat_night_A", "cat_night_B", "cat_day_A", "cat_day_B"))
+  # expect_equal(ap$factors, 3)
   expect_equal(ap$mu, 1:8)
   expect_equal(ap$sd, 1:8)
   expect_equal(ap$r, c(0, 0.05, 0.15, 0.10, 0.20, 0.25, 
@@ -74,8 +76,9 @@ test_that("2w*2w*2b", {
                        0.30, 0, 0, 0, 0, 0.35, 0.45, 
                        0, 0, 0, 0, 0.40, 0.50, 0.55))
   expect_equal(ap$n, 100)
-  expect_equal(ap$design_factors, c(0,1,1))
+  #expect_equal(ap$design_factors, c(0,1,1))
   expect_equal(ap$labelnames, c("pet", "dog", "cat",
                                 "time", "night", "day", 
                                 "condition", "A", "B"))
+  expect_equal(ap$plot, FALSE)
 })
